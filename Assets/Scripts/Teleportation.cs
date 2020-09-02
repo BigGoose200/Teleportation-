83eba0c9aff729ccfca28e53af5404d7d1162f91
BigGoose200 <60943774+BigGoose200@users.noreply.github.com>
2020-09-02 01:07:49 -0400

    added assets

---
+++ Assets/Scripts/Teleportation.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Teleportation : MonoBehaviour
+{
+    // declaring objects
+    public GameObject Portal; 
+    public GameObject Player;
+
+    public void OnTriggerEnter2D(Collider2D other)
+    {
+        // when the portal collides with object tagged "player," the object teleports
+        if (other.gameObject.tag == "Player")
+        {
+            StartCoroutine(Teleport ());
+        }
+        
+    }
+
+    IEnumerator Teleport() // function that activate teleport
+    {
+        yield return new WaitForSeconds(0.5f); // time it takes when player teleports
+        Player.transform.position = new Vector2(Portal.transform.position.x, Portal.transform.position.y); // allows player to be moved into a different location
+    }
+
+}
